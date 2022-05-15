5c0cbc840dd0264cbde7ecaba42ce5f149eb54fb
Aidan Baird <102356055+AidanBaird@users.noreply.github.com>
2022-05-15 17:41:33 +0100

    Add files via upload

---
+++ Chapter_three_A_joke_telling_program.py
@@ -0,0 +1,22 @@
+# Chapter three revolves around a program that returns jokes to the the reader
+
+# Key terms are escape characters, single/ double line quotes and printing
+
+print("What do you get when you cross a snowman with a vampire?")
+input()
+print("Frostbite!")
+print("")
+print("What do dentists call an astronaut\'s cavity?")
+input()
+print("A black hole!")
+print()
+print("Knock knock")
+input()
+print("Who's there?")
+input()
+print("Interrupting cow")
+input()
+print("Interrupting cow wh", end="")
+print("-MOO")
+
+# Shows the use cases for either " or ' and introduces the "end" keyword
